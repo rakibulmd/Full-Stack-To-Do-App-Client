82be39a58e8fa6ada5359dc0035cf2241455fdf9
Rakibul md <rakib245875@gmail.com>
2022-05-19 05:12:39 +0600

    added tasks, delete and complete button

--- src/Pages/AddTask/AddTask.js
+++ src/Pages/AddTask/AddTask.js
@@ -1,9 +1,105 @@
+import axios from "axios";
 import React from "react";
-
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import auth from "../../firebase.init";
 const AddTask = () => {
+    const [user] = useAuthState(auth);
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+        reset,
+    } = useForm();
+    const onSubmit = (data) => {
+        data.status = "incomplete";
+        data.email = user?.email;
+        console.log(data);
+        const post = async () => {
+            await axios
+                .post("http://localhost:5000/addTask", data)
+                .then(toast.success("Task add successful"));
+        };
+        post();
+        reset();
+    };
     return (
-        <div>
-            <h2>Add new task</h2>
+        <div className="container mx-auto">
+            <h2 className="text-2xl font-bold uppercase pt-10 text-center">
+                Add new task:
+            </h2>
+            <div className="max-w-[500px] mx-auto py-5">
+                <form
+                    className="p-5 bg-gray-800 rounded-md border border-emerald-500 animate__animated animate__fadeInUp animate__faster"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    <div className="mb-7">
+                        <label
+                            htmlFor="name"
+                            className="block mb-2 text-sm font-medium text-gray-300"
+                        >
+                            Task Name:
+                        </label>
+                        <input
+                            id="name"
+                            className="border   text-sm rounded-md focus:ring-teal-500  block w-full p-2.5 bg-gray-700 placeholder-gray-400 text-white  border-teal-500"
+                            placeholder="Task name here"
+                            type="text"
+                            autoComplete="off"
+                            {...register("taskName", {
+                                minLength: 3,
+                                required: true,
+                            })}
+                        />
+                        {errors.taskName && (
+                            <span className="text-rose-600">
+                                Minimum 3 letters
+                            </span>
+                        )}
+                    </div>
+
+                    <div className="mb-7">
+                        <label
+                            htmlFor="description"
+                            className="block mb-2 text-sm font-medium text-gray-300"
+                        >
+                            Task Description:
+                        </label>
+                        <textarea
+                            id="description"
+                            className="border   text-sm rounded-md focus:ring-teal-500  block w-full p-2.5 bg-gray-700 placeholder-gray-400 text-white  border-teal-500"
+                            placeholder="Task description here"
+                            type="text"
+                            autoComplete="off"
+                            {...register("taskDescription", {
+                                minLength: 10,
+                                required: true,
+                            })}
+                        />
+                        {errors.taskDescription && (
+                            <span className="text-rose-600">
+                                Description at least 10 letters
+                            </span>
+                        )}
+                    </div>
+
+                    <input
+                        className="w-full bg-emerald-400 hover:bg-emerald-500 active:bg-emerald-600 px-5 py-2 rounded-md text-black tracking-wide"
+                        type="submit"
+                        value="Add Item"
+                    />
+                </form>
+                <div className="mx-auto py-5">
+                    <Link
+                        className="w-full bg-emerald-400 hover:bg-emerald-500 active:bg-emerald-600 px-5 py-2 rounded-md text-black tracking-wide"
+                        to="/"
+                    >
+                        Go to home
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/ToDo.js
+++ src/Pages/Home/ToDo.js
@@ -1,14 +1,94 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const ToDo = () => {
+    const [user] = useAuthState(auth);
+    const [changed, setChanged] = useState(false);
     const navigate = useNavigate();
     const handleAddTaskBtn = () => {
         navigate("/addTask");
     };
+    const [tasks, setTasks] = useState([]);
+    useEffect(() => {
+        const get = async () => {
+            await axios
+                .get(`http://localhost:5000/myTasks?email=${user?.email}`)
+                .then((response) => {
+                    setTasks(response.data);
+                });
+        };
+        get();
+    }, [user, changed]);
+    const handleCompleteBtn = async (id) => {
+        console.log(id);
+        await axios
+            .put(`http://localhost:5000/myTasks/${id}`)
+            .then((response) => {
+                setChanged(!changed);
+            });
+    };
+    const handleDeleteBtn = async (id) => {
+        const proceed = window.confirm("Are your sure to delete?");
+        if (proceed) {
+            await axios.delete(`http://localhost:5000/myTasks/${id}`);
+            const rest = tasks.filter((task) => task._id !== id);
+            setTasks(rest);
+        }
+    };
     return (
-        <div className="container mx-auto p-2s py-10">
+        <div className="container mx-auto p-2 py-10">
             <h2 className="text-center">Your Tasks</h2>
+            <h2>{tasks?.length}</h2>
+            {tasks.map((task) => (
+                <div
+                    className="p-5 bg-gray-800 rounded-lg max-w-[700px] mx-auto py-5 mb-5"
+                    key={task._id}
+                >
+                    <div className="grid grid-cols-1 md:grid-cols-2 justify-between items-center">
+                        <div className="mb-3">
+                            <h2
+                                className={
+                                    task.status === "completed"
+                                        ? `text-xl text-gray-500 line-through`
+                                        : `text-xl text-emerald-500`
+                                }
+                            >
+                                Task: {task.taskName}
+                            </h2>
+                            <p
+                                className={
+                                    task.status === "completed"
+                                        ? `text-xl text-gray-500 line-through`
+                                        : `text-xl text-white`
+                                }
+                            >
+                                Description: {task.taskDescription}
+                            </p>
+                        </div>
+                        <div>
+                            <button
+                                onClick={() => handleCompleteBtn(task?._id)}
+                                className={
+                                    task.status === "completed"
+                                        ? `btn btn-disabled text-gray-500 mr-5`
+                                        : `btn btn-success mr-5`
+                                }
+                            >
+                                Completed
+                            </button>
+                            <button
+                                onClick={() => handleDeleteBtn(task?._id)}
+                                className="btn btn-error"
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            ))}
             <div className="flex justify-center">
                 <button
                     onClick={handleAddTaskBtn}
