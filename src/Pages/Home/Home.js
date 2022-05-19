673e2b6443adca0a61bdad85490aacc34e04682c
Rakibul md <rakib245875@gmail.com>
2022-05-19 06:09:15 +0600

    updated favicon and added loader in homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
+import Loading from "../Shared/Loading/Loading";
 import Banner from "./Banner";
 import ToDo from "./ToDo";
 
 const Home = () => {
-    const [user] = useAuthState(auth);
+    const [user, loading] = useAuthState(auth);
+    if (loading) {
+        return (
+            <div className="w-40 h-40 mx-auto">
+                <Loading></Loading>
+            </div>
+        );
+    }
     return <div>{user ? <ToDo></ToDo> : <Banner></Banner>}</div>;
 };
 
